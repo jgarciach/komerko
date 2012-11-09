76306b8af18d27dd2f5b751b8097ad21971ca246
jgarciach <jgarciach@gmail.com>
2012-11-08 20:45:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Komerko::Application.config.secret_token = '09eb3fa22f9fa6c41288ddbb73e14a611772c6119ce151356c42a3dd9e422d3a1d9ae31431c67ccfb34a7fbd6491969257996079a83a16f9aa72bee5d47b08bb'
