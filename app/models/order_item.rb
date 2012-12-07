a6375b7eb50ac5fa6c1ebffac9a0561dfd1b7203
jgarciach <jgarciach@gmail.com>
2012-12-07 17:53:55 -0500

    Added create order items method, which takes cart_items and an order and creates order items

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,10 @@
 class OrderItem < ActiveRecord::Base
   attr_accessible :item_id, :order_id, :price_paid_per_unit, :quantity
+ 
+  def create_order_items(order_id, cart_items)
+    for cart_items.each do |cart_item|
+      item = cart_item.item
+      OrderItem.create(item_id: item.id, order_id: order_id, price_paid_per_unit: item.price, quantity: cart_item.quantity)
+    end
+  end
 end
