77b1ebc7212e259a92e9878e464d4419281183d7
jgarciach <jgarciach@gmail.com>
2012-12-09 17:45:53 -0500

    Created funciton to toggle address fields depending on whether order is delivery or pickup.

---
+++ app/assets/javascripts/orders.js
@@ -0,0 +1,21 @@
+//# Place all the behaviors and hooks related to the matching controller here.
+//# All this logic will automatically be available in application.js.
+//# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+function toggleAddressFields () {
+  if ($("#order_order_type_pickup").is(':checked')) {
+    $(".address-field").hide();
+  }
+  else if ($("#order_order_type_delivery").is(':checked')) {
+    $(".address-field").show();
+  }
+}
+$(function() {
+  toggleAddressFields();
+  $("#order_order_type_delivery").click(function() {
+    toggleAddressFields();
+  });
+  $("#order_order_type_pickup").click(function() {
+    toggleAddressFields();
+  });
+});
