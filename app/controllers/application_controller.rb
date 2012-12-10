5279c6119d1a2b11700b473305a3e15313407f01
Jose Varela <joservarelaf@gmail.com>
2012-12-10 00:39:53 -0500

    Session takes into account user signed in when assigning cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   def get_cart_id(business_id)
     cart_id = "cart_" + String(business_id)
     unless session[cart_id]
-      session[cart_id] = Cart.create(business_id: business_id).id
+      new_cart = Cart.new(business_id: business_id)
+      if user_signed_in?
+        new_cart.user_id = current_user.id
+      end
+      session[cart_id] = new_cart.save.id
     end
     session[cart_id]
   end
