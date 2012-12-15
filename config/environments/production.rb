3d16bbe0eb7cb2682b46d02874d8f36c7ff2dc11
jgarciach <jgarciach@gmail.com>
2012-12-14 22:51:20 -0500

    Test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Komerko::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(third_party.css store.css table.css bootstrap_and_overrides.css scaffolds.css)
+  config.assets.precompile += %w(third_party.css store.css table.css bootstrap_and_overrides.css scaffolds.css checkout.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
